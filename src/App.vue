7311b2930d2bbc832723974a39a01c6fe09d340e
RukawaY <rukawayuan@outlook.com>
2025-04-27 14:35:35 +0800

    api initialize

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 import { ref, provide, onMounted } from 'vue';
 import TopBar from './components/TopBar.vue'
 
-const user = ref('admin');
+const user = ref('student');
 const user_name = ref('用户名');
 const user_avatar = ref('https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png');
 const is_homepage = ref(true);
